440508488d18cb3981b2f7f45d018573218487ac
rmingon <ronanmingon69550@gmail.com>
2024-08-07 16:22:41 +0200

    wifi + send data to srv

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,31 +1,71 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+WiFiUDP udp;
+
 #include <WiiChuck.h>
+
+
+#include "config.h"
+#include <ArduinoJson.h>
+
+char packetBuffer[2000];
 
 Accessory nunchuck;
 
+String uniq = "";
+void getMac() {
+  byte mac[6];
+  WiFi.macAddress(mac);
+  uniq = String(mac[0],HEX) +String(mac[1],HEX) +String(mac[2],HEX) +String(mac[3],HEX) + String(mac[4],HEX) + String(mac[5],HEX);
+}
+
+void sendDataToServer(String type, JsonDocument data) {
+  JsonDocument doc;
+  doc["uniq"] = uniq;
+  doc["type"] = type;
+  doc["data"] = data;
+  udp.beginPacket(SERVER_HOST_NAME, UDP_PORT);
+  serializeJson(doc, udp);
+  udp.endPacket();
+}
 
 void setup() {
   Serial.begin(115200);
   nunchuck.begin();
 	if (nunchuck.type == Unknown) {
-		/** If the device isn't auto-detected, set the type explicatly
-		 * 	
-		 NUNCHUCK,
-		 WIICLASSIC,
-		 GuitarHeroController,
-		 GuitarHeroWorldTourDrums,
-		 DrumController,
-		 DrawsomeTablet,
-		 Turntable
-		 */
 		nunchuck.type = NUNCHUCK;
 	}
+  getMac();
+
+  WiFi.begin(SSID, PASSWORD);
+  for(int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
+      Serial.print(".");
+      delay(1000);
+  }
+
+  udp.begin(UDP_PORT);
+  JsonDocument data;
+  sendDataToServer("controller", data);
 
 }
 
 void loop() {
-  Serial.println("-------------------------------------------");
-  nunchuck.readData();    // Lire les entrees et les ecrire dans values[]
+  nunchuck.readData();
+  JsonDocument motion;
+  
+  motion["JoyX"] = nunchuck.values[0];
+  motion["JoyY"] = nunchuck.values[1];
+  motion["RollAngle"] = nunchuck.values[2];
+  motion["PitchAngle"] = nunchuck.values[3];
+  motion["AccX"] = nunchuck.values[4];
+  motion["AccY"] = nunchuck.values[5];
+  motion["AccZ"] = nunchuck.values[6];
+  motion["BoutonZ"] = nunchuck.values[10];
+  motion["BoutonC"] = nunchuck.values[11];
+
+  sendDataToServer("motion", motion);
+
   Serial.print("JoyX = ");Serial.println(nunchuck.values[0]);//Joystick axe X
   Serial.print("JoyY = ");Serial.println(nunchuck.values[1]);//Joystick axe Y
   Serial.print("RollAngle = ");Serial.println(nunchuck.values[2]);//Angle de roulis
@@ -35,5 +75,5 @@ void loop() {
   Serial.print("AccZ = ");Serial.println(nunchuck.values[6]);//Acceleration axe Z
   Serial.print("BoutonZ = ");Serial.println(nunchuck.values[10]);//Bouton Z
   Serial.print("BoutonC = ");Serial.println(nunchuck.values[11]);//Bouton C
-  delay(200);
+  delay(25);
 }
